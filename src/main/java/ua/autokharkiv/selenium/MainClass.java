01aa5ccb56b766fbe9b4e44aff206cb50234b0f7
sfanpro <st.sergeyf@gmail.com>
2019-03-26 15:01:38 +0200

    added comments to MainClass

--- src/main/java/ua/autokharkiv/selenium/MainClass.java
+++ src/main/java/ua/autokharkiv/selenium/MainClass.java
@@ -8,20 +8,27 @@ import java.util.concurrent.TimeUnit;
 
 public class MainClass {
     public static void main( String[] args ) {
+        //  иницилизируем chrome драйвер
         WebDriver driver = new ChromeDriver();
+        // задаем неявные ожидания
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        // разворачиваем окно на весь экран
         driver.manage().window().maximize();
 
+        // устанавливаем подключение к сайту
         driver.get("https://auto.kharkiv.ua/");
 
+        // инициализируем главную страницу, страницу регистрации и страницу входа
 //        MainPage mainPage = new MainPage(driver);
         MainPage mainPage = PageFactory.initElements(driver, MainPage.class);
         RegisterPage registerPage = new RegisterPage(driver);
         LoginPage loginPage = new LoginPage(driver);
 
+        // пробуем зарегестрироваться с неверными данными
         mainPage.clickRegister();
         registerPage.registerWithInvalidInput("qwer", "qwe", "qwe@123", "qwe");
 
+        // пробуем залогинитьтся с неверными данными
         mainPage.clickLogin();
         loginPage.loginWithInvalidInput("asd", "asd");
     }
